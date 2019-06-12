77798cc9d404311e0d515e85b0a69ec49af41c2e
Mai Duc Tuan <tuan.md@teko.vn>
2019-06-12 15:39:06 +0700

    1

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('get_cinema_by_location', views.get_cinema_by_location, name='get_cinema_by_location'),
+    path('get_cinema_by_location', views.get_cinema, name='get_cinema'),
 ]
--- api/views.py
+++ api/views.py
@@ -4,5 +4,5 @@ from django.http import JsonResponse
 from api.services import get_cinema_by_location
 
 
-def get_cinema_by_location(request):
+def get_cinema(request):
     return JsonResponse(get_cinema_by_location.call(93))
